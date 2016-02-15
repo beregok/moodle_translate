645fd96e6e4c55d1cc0154f51ee9d3a643d152e1
Alexandr Berejnoy <7ya.beregni@gmail.com>
2016-02-15 21:54:26 +0200

    Create block_xp.php

---
+++ blocks/xp/lang/uk/block_xp.php
@@ -0,0 +1,179 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package    block_xp
+ * @copyright  2014 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activityoresourceis'] = 'Діяльність або ресурс {$a}';
+$string['addacondition'] = 'Додати умову';
+$string['addarule'] = 'Додати правило';
+$string['addrulesformhelp'] = 'В останньому стовпчику визначається кількість отриманих очок досвіду, коли умови задовольняються.';
+$string['awardaxpwhen'] = '<strong>{$a}</strong>  балів досвіду заробляється коли:';
+$string['basexp'] = 'База алогритмів';
+$string['changelevelformhelp'] = 'Якщо змінити число рівнів, знаки для користувача рівню буде тимчасово відключено, щоб запобігти рівню без пропусків. Якщо змінити підрахунок рівня перейти і на сторінку \'Вигляд\'повторно включити призначені для користувача значки після збереження цієї форми.';
+$string['cachedef_filters'] = 'Фільтр рівнів';
+$string['cachedef_ruleevent_eventslist'] = 'Список деяких подій';
+$string['cheatguard'] = 'Захист від шахрайства';
+$string['colon'] = '{$a->a}: {$a->b}';
+$string['coefxp'] = 'Коефіцієнт алгоритму';
+$string['configdescription'] = 'Опис для додавання';
+$string['configheader'] = 'Налаштування';
+$string['configtitle'] = 'Назва';
+$string['congratulationsyouleveledup'] = 'Вітаємо !';
+$string['coolthanks'] = 'Клас, так тримати!';
+$string['courselog'] = 'Події курсу';
+$string['coursereport'] = 'Звіт курсу';
+$string['courserules'] = 'Правила курсу';
+$string['coursesettings'] = 'Налаштування курсу';
+$string['coursevisuals'] = 'Змінити оформлення';
+$string['customizelevels'] = 'Змінити рівні';
+$string['defaultrules'] = 'Правила за замовчуванням';
+$string['defaultrulesformhelp'] = 'Такі правила за замовчуванням, наданим плагіном, вони автоматично дають замовчуванням бали досвіду і ігнорують деякі надлишкові події. Ваші власні правила мають пріоритет над ними.';
+$string['deletecondition'] = 'Видалити умову';
+$string['deleterule'] = 'Видалити правило';
+$string['description'] = 'Опис';
+$string['dismissnotice'] = 'Відхилити повідомлення';
+$string['enableinfos'] = 'Включити сторінку з інформацією';
+$string['enableinfos_help'] = 'При уставновці \'Ні\', студенти не зможуть переглядати інформаційну сторінку.';
+$string['enableladder'] = 'Включити лідерів';
+$string['enableladder_help'] = 'При установці \'Ні\', студенти не зможуть бачити лідерів.';
+$string['enablelevelupnotif'] = 'Включити інформування про підвищення рівня';
+$string['enablelevelupnotif_help'] = 'При установці \'Так\', Студентам буде відображатися спливаюче вікно з привітанням досягнутого нового рівня.';
+$string['enablelogging'] = 'Включити записи подій';
+$string['enablexpgain'] = 'Включити бали';
+$string['enablexpgain_help'] = 'При установці \'Ні\', ніхто не буде заробляти бали досвіду в курсі. Це корисно, щоб заморозити бали, накопичені або включити їх в певний момент часу.
+
+Будь ласка, зверніть увагу, що це також може управлятися точніше налаштовуватися використовувати можливості \'block/xp:earnxp\'.';
+$string['errorformvalues'] = 'Є деякі помилки, в значеннях форм, будь ласка виправити їх.';
+$string['errorlevelsincorrect'] = 'Мінімальна кількість рівнів 2';
+$string['errornotalllevelsbadgesprovided'] = 'Не всі бейджи для рівнів були надані. відсутні: {$a}';
+$string['errorunknownevent'] = 'Помилка: невідома подія';
+$string['errorunknownmodule'] = 'Помилка: Невідомий модуль';
+$string['errorxprequiredlowerthanpreviouslevel'] = 'Балів потрібно менше або дорівнює попередньому рівню.';
+$string['eventis'] = 'Подія {$a}';
+$string['event_user_leveledup'] = 'Рівень досягнень користувача';
+$string['eventname'] = 'Назва події';
+$string['eventproperty'] = 'Властивість події';
+$string['eventtime'] = 'Завжди';
+$string['for1day'] = 'Протягом 1 дня';
+$string['for1month'] = 'Протягом місяця';
+$string['for1week'] = 'Протягом тижня';
+$string['for3days'] = 'Протягом 3 днів';
+$string['forever'] = 'Назавжди';
+$string['forthewholesite'] = 'За ввесь сайт';
+$string['give'] = 'give';
+$string['incourses'] = 'У курсах';
+$string['infos'] = 'Шкала';
+$string['invalidxp'] = 'Невірне значення балів';
+$string['keeplogs'] = 'Зберігади журнали';
+$string['ladder'] = 'Рейтинг';
+$string['level'] = 'Рівень';
+$string['levelbadges'] = 'Значки досягнень';
+$string['levelbadgesformhelp'] = 'Назвіть файли [назва].[розширення], наприклад: 1.png, 2.jpg, etc... Рекомендований розмір 100x100.';
+$string['levelcount'] = 'Лічильник рівня';
+$string['leveldesc'] = 'Опис рівня';
+$string['levels'] = 'Рівні';
+$string['levelswillbereset'] = 'Увага! Збереження цієї форми перераховує всі рівні!';
+$string['levelup'] = 'Рівень Досягнень!';
+$string['levelx'] = 'Рівень #{$a}';
+$string['likenotice'] = '<strong>Чи подобається вам плагін?</strong> Будь-ласка знайдіть час щоб <a href="{$a->moodleorg}" target="_blank">додати до закладок</a> Moodle.org і <a href="{$a->github}" target="_blank">поставити зірку на GitHub</a>.';
+$string['logging'] = 'Записи подій';
+$string['maxactionspertime'] = 'Макс.кількість дій';
+$string['maxactionspertime_help'] = 'Максимальна кількість дій, які матимуть значення балів за вказаний період часу заданого. При будь-якому буде проігноровано.';
+$string['movecondition'] = 'Посунути значення';
+$string['moverule'] = 'Посунути правило';
+$string['navinfos'] = 'Інформація';
+$string['navladder'] = 'Лідер';
+$string['navlevels'] = 'Рівні';
+$string['navlog'] = 'Записи';
+$string['navreport'] = 'Звіти';
+$string['navrules'] = 'Правила';
+$string['navsettings'] = 'Налаштування';
+$string['navvisuals'] = 'Вигляд';
+$string['pickaconditiontype'] = 'Виберіть тип умови';
+$string['pluginname'] = 'Рівень Досягнень!';
+$string['progress'] = 'Досягнення';
+$string['property:action'] = 'Дії з подіями';
+$string['property:component'] = 'Компонент дія';
+$string['property:crud'] = 'Подія CRUD (Створення, читання, оновлення, видалення)';
+$string['property:eventname'] = 'Назва події';
+$string['property:target'] = 'Мета події';
+$string['participatetolevelup'] = 'Практикуйся та навчайся щоб отриматити бали для підвищення свого професйного рівня!';
+$string['rank'] = 'Місце';
+$string['reallyresetdata'] = 'Дійсно скинути рівні та бали досвіду кожної людини в цьому курсі?';
+$string['reallyresetgroupdata'] = 'Дійсно скинути рівні та бали досвіду кожної людини в цій групі?';
+$string['resetcoursedata'] = 'Скинути дані курсу';
+$string['resetgroupdata'] = 'Скинути дані групи';
+$string['rule'] = 'Правило';
+$string['rule:contains'] = 'містить';
+$string['rule:eq'] = 'дорівнює';
+$string['rule:eqs'] = 'строго дорівнює';
+$string['rule:gt'] = 'більше';
+$string['rule:gte'] = 'більше або дорівнює';
+$string['rule:lt'] = 'менше';
+$string['rule:lte'] = 'менше або дорівнює';
+$string['rule:regex'] = 'відповідає формальним виразам';
+$string['rulecm'] = 'Активність або ресурс';
+$string['rulecmdesc'] = 'Діяльність або ресурс \'{$a->contextname}\'.';
+$string['ruleevent'] = 'Конкретна подія';
+$string['ruleeventdesc'] = 'Подія \'{$a->eventname}\'';
+$string['ruleproperty'] = 'Властивості події';
+$string['rulepropertydesc'] = 'Властивість \'{$a->property}\' {$a->compare} \'{$a->value}\'.';
+$string['ruleset'] = 'Набір умов';
+$string['ruleset:all'] = 'Всі умови істинні';
+$string['ruleset:any'] = 'Будь-які умови, вірні';
+$string['ruleset:none'] = 'Жодна з умов не є істинна';
+$string['rulesformhelp'] = '<p>Цей плагін виконує нарахування балів за отриманий досвід та викоонання завдань на курсі студентами. Ви можете використовувати форму нижче, щоб додати свої власні правила і переглядати конфігурації за замовчуванням. .</p>
+<p>Рекомендується перевірити плагін \'и <a href="{$a->log}">за подіями</a> щоб визначити, які події викликали, як ви виконувати дії в ході, а також для читання більше про самих подій: <a href="{$a->list}">Список подій</a>, <a href="{$a->doc}">документація розробника</a>.</p>
+<p>Нарешті, зверніть увагу, що плагін завжди ігнорує:
+<ul>
+    <li>Дії, що виконуються адміністраторами, гостей або не зареєстрованих користувачів.</li>
+    <li>Дії, що виконуються користувачами, які не мають можливості <em>блок/xp:заробити</em>.</li>
+    <li> Повторні дії протягом короткого проміжку часу, щоб запобігти шахрайству.</li>
+    <li>Події, позначені як <em>невідомі</em>, наприклад анонімний зворотній зв’язок.</li>
+    <li>Події освітнього рівня, не рівня <em>Участі</em>.</li>
+</ul>
+</p>';
+$string['timebetweensameactions'] = 'Час, необхідний між однаковими діями';
+$string['timebetweensameactions_help'] = 'Протягом декількох секунд, мінімальний час, необхідний між однаковими діями. Дія вважається однаковим, якщо вона була поміщена в тому ж контексті і об’єкта, читання повідомлення в форумі буде розглядатися як ідентичні, якщо той же пост знову читати.';
+$string['timeformaxactions'] = 'Терміни для макс. дії';
+$string['timeformaxactions_help'] = 'Терміни (в секундах), протягом якого користувач не повинен перевищувати максимальну кількість дій.';
+$string['value'] = 'Змінні';
+$string['valuessaved'] = 'Змінні були успішно збережені.';
+$string['viewtheladder'] = 'Хто лідер?';
+$string['wherearexpused'] = 'Де бали досвіду викоритовувались?';
+$string['wherearexpused_desc'] = 'При установці на \'В курсах\', значення накопичених балів досвіду буде враховуватися тільки, звичайно, в якій курс був доданий. При установці на \'для всього сайту\', користувач буде "Рівнем вище" на сайті, а не вибірково на курс, буде використовуватися весь досвід, накопичений по всьому сайту.';
+$string['updateandpreview'] = 'Оновити та переглянути';
+$string['usealgo'] = 'Використовувати алгоритми';
+$string['usecustomlevelbadges'] = 'Використовувати власні бейджи';
+$string['usecustomlevelbadges_help'] = 'Коли встановлено Так, ви повинні надати оригінал зображення для кожного рівня.';
+$string['when'] = 'Коли';
+$string['xp'] = 'Набрані бали';
+$string['xp:addinstance'] = 'Додати блок балів';
+$string['xp:earnxp'] = 'Заробіток балів досвіду';
+$string['xp:myaddinstance'] = 'Додати блок на мою власну панель';
+$string['xp:view'] = 'Переглянути блок і його сторінки';
+$string['xprequired'] = 'Потрібно балів';
+$string['xpgaindisabled'] = 'Бали відключені';
+$string['youreachedlevela'] = 'Ви досягли рівня {$a}!';
+$string['yourownrules'] = 'Свої власні правила';
