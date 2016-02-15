9187b5148efd86da2785e26f90e867cfed44441d
Alexandr Berejnoy <7ya.beregni@gmail.com>
2016-02-15 21:51:04 +0200

    Create game.php

---
+++ mod/game/lang/uk/game.php
@@ -0,0 +1,351 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+// Translated by Vasilis Daloukas.
+
+// File bookquiz/importodt.php.
+$string[ 'bookquiz_not_select_book'] = 'Ви не обрали книгу';
+
+// File bookquiz/play.php.
+$string[ 'bookquiz_empty'] = 'Книга порожня';
+$string[ 'sudoku_submit'] = 'Оцінка відповідей';
+
+// File bookquiz/questions.php.
+$string[ 'bookquiz_categories'] = 'Категорія';
+$string[ 'bookquiz_chapters'] = 'Розділи';
+$string[ 'bookquiz_numquestions'] = 'Запитання';
+
+// Classes.
+$string[ 'eventgamecreated'] = 'Гра створена';
+$string[ 'eventgamedeleted'] = 'Гра видалена';
+$string[ 'eventgamesupdated'] = 'Гра оновлена';
+$string[ 'eventgameviewed'] = 'Гра переглянута';
+$string[ 'eventgameplayed'] = 'Зіграні ігри';
+
+// File cross/cross_class.php.
+$string[ 'lettersall'] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+// File cross/crossdb_class.php.
+$string[ 'and'] = 'і';
+$string[ 'cross_correct'] = 'правильний символ';
+$string[ 'cross_corrects'] = 'правильні літери';
+$string[ 'cross_error'] = 'неправльна буква';
+$string[ 'cross_errors'] = 'неправльне слово';
+$string[ 'cross_found_many'] = 'Пошук';
+$string[ 'cross_found_one'] = 'Пошук';
+$string[ 'grade'] = 'Оцінка';
+$string[ 'cross_disabletransformuppercase'] = 'Відключити претвонення тесту в  прописний через CSS';
+
+// File cross/play.php.
+$string[ 'cross_across'] = 'По-вертикалі';
+$string[ 'cross_checkbutton'] = 'Перевірити відповіді';
+$string[ 'cross_down'] = 'По-горизонталі';
+$string[ 'cross_endofgamebutton'] = 'Закінчити розгадувати';
+$string[ 'cross_error_containsbadchars'] = 'Слово містить неприпустимі символи';
+$string[ 'cross_error_wordlength1'] = 'Правильне слово містить ';
+$string[ 'cross_error_wordlength2'] = ' літер.';
+$string[ 'cross_pleasewait'] = 'Будь ласка, зачекайте, поки кросворд завантажується';
+$string[ 'cross_welcome'] = '<h3> Ласкаво просимо!</h3><p>Натисніть на слово, щоб почати / продовжити.</p>';
+$string[ 'letter'] = 'літера';
+$string[ 'letters'] = 'літер';
+$string[ 'nextgame'] = 'Нова гра';
+$string[ 'no_words'] = 'Немає жодного слова';
+$string[ 'print'] = 'Друк';
+$string[ 'win'] = 'Вітаємо !!!';
+
+// File cryptex/play.php.
+$string[ 'finish'] = 'Кінець гри';
+
+// File db/access.php.
+$string[ 'game:addinstance'] = 'Додати нову гру';
+$string[ 'game:attempt'] = 'Грати';
+$string[ 'game:deleteattempts'] = 'Видалення спроб';
+$string[ 'game:grade'] = 'Оцінити гру вручну';
+$string[ 'game:manage'] = 'Керувати';
+$string[ 'game:manageoverrides'] = 'Перевизначити управління грою';
+$string[ 'game:preview'] = 'Попередній перегляд ігор';
+$string[ 'game:reviewmyattempts'] = 'огляд спроби';
+$string[ 'game:view'] = 'вигляд';
+$string[ 'game:viewreports'] = 'переглянути звіти';
+
+// File hangman/play.php.
+$string[ 'hangman_correct_phrase'] = 'Правильна фраза була: ';
+$string[ 'hangman_correct_word'] = 'Правильне слово було: ';
+$string[ 'hangman_gradeinstance'] = 'Оцінка гри в цілому';
+$string[ 'hangman_letters'] = 'Літери: ';
+$string[ 'hangman_restletters_many'] = 'Ви маєте <b>{$a}</b> спроб';
+$string[ 'hangman_restletters_one'] = 'Ви маєте <b>Тільки 1</b> спробу';
+$string[ 'hangman_wrongnum'] = 'Неправильно: %d із %d';
+$string[ 'nextword'] = 'Наступне слово';
+
+// File hiddenpicture/play.php.
+$string[ 'hiddenpicture_mainsubmit'] = 'Оцінка головного питання';
+$string[ 'hiddenpicture_nocols'] = 'Повинні вказати кількість рядків по горизонталі';
+$string[ 'hiddenpicture_nomainquestion'] = 'Немає глосарію з таким {$a->name} в якому прикріплене зображення';
+$string[ 'hiddenpicture_norows'] = 'HПотрібно вказати кількість рядків по вертикалі';
+$string[ 'must_select_glossary'] = 'Ви повинні вибрати глосарій';
+$string[ 'no_questions'] = "Там немає питань";
+$string[ 'noglossaryentriesfound'] = 'Не знайдено жодної статті в глосарії';
+
+// File millionaire/play.php.
+$string[ 'millionaire_must_select_questioncategory'] = 'Ви повинні вибрати одну категорію питання';
+$string[ 'millionaire_must_select_quiz'] = 'Ви повинні вибрати одну вікторину запитань';
+
+// File report/overview/report.php.
+$string[ 'allattempts'] = 'Показати всі спроби';
+$string[ 'allstudents'] = 'Показати $a';
+$string[ 'attemptduration'] = 'Тривалість спроб';
+$string[ 'attemptsonly'] = 'Показати тільки спроби учнів';
+$string[ 'deleteattemptcheck'] = 'Ви абсолютно впевнені, що хочете повністю видалити ці спроби??';
+$string[ 'displayoptions'] = 'Параметри відображення';
+$string[ 'downloadods'] = 'Завантажити в ODS форматі';
+$string[ 'feedback'] = 'Зворотній зв’язок';
+$string[ 'noattemptsonly'] = 'Показати тільки $a без будь-яких спро';
+$string[ 'numattempts'] = '$a-> всього студентів, $a-> студентів склали успішно, та $a->спробували';
+$string[ 'pagesize'] = 'Питання на сторінці:';
+$string[ 'reportoverview'] = 'Oгляд';
+$string[ 'selectall'] = 'Вибрати все';
+$string[ 'selectnone'] = 'Не вибирати нічого';
+$string[ 'showdetailedmarks'] = 'Показати всі деталі';
+$string[ 'startedon'] = 'Процес почався';
+$string[ 'timecompleted'] = 'Завершено';
+$string[ 'unfinished'] = 'відкрити';
+$string[ 'withselected'] = 'Обрані';
+
+// File snakes/play.php.
+$string[ 'snakes_dice'] = 'На кубику випало $a.';
+$string[ 'snakes_player'] = 'Позиція гравця: $a.';
+
+// File sudoku/create.php.
+$string[ 'sudoku_create_count'] = 'Кількість створних судоку';
+$string[ 'sudoku_create_start'] = 'Початок створення судоку';
+$string[ 'sudoku_creating'] = 'Створення <b>{$a}</b> судоку';
+
+// File sudoku/play.php.
+$string[ 'sudoku_finishattemptbutton'] = 'Кінець гри';
+$string[ 'sudoku_guessnumber'] = 'Вгадайте правильний номер';
+$string[ 'sudoku_noentriesfound'] = 'Не знайдено слів в глосарії';
+
+// File export.php.
+$string[ 'export'] = 'Експорт';
+$string[ 'html_hascheckbutton'] = 'Має кнопку перевірки:';
+$string[ 'html_hasprintbutton'] = 'Має кнопку друку:';
+$string[ 'html_title'] = 'Заголовок для html:';
+$string[ 'javame_createdby'] = 'Створений:';
+$string[ 'javame_description'] = 'Опис:';
+$string[ 'javame_filename'] = 'Ім’я файлу:';
+$string[ 'javame_icon'] = 'Логотип:';
+$string[ 'javame_maxpictureheight'] = 'Максимальна висота зображення:';
+$string[ 'javame_maxpicturewidth'] = 'Максимальна ширина зображення:';
+$string[ 'javame_name'] = 'Ім’я:';
+$string[ 'javame_type'] = 'Тип:';
+$string[ 'javame_vendor'] = 'Розробник:';
+$string[ 'javame_version'] = 'Версія:';
+
+// File exporthtml_hangman.php.
+$string[ 'hangman_loose'] = '<BIG><B>Гра закінчена</B></BIG>';
+$string[ 'html_hangman_new'] = 'Нова';
+
+// File exporthtml_millionaire.php.
+$string[ 'millionaire_helppeople'] = 'Допомога людей';
+$string[ 'millionaire_info_people'] = 'Люди кажуть';
+$string[ 'millionaire_info_telephone'] = 'Я думаю, що правильна відповідь ';
+$string[ 'millionaire_info_wrong_answer'] = 'Ваша відповідь невірна <br>Правильна відповдь:';
+$string[ 'millionaire_quit'] = 'Вийти';
+$string[ 'millionaire_sourcemodule_must_quiz_question'] = 'Для мільйонера кількість питань повинна бути {$a} або це не питання';
+$string[ 'millionaire_telephone'] = 'Подзвонити другу';
+$string[ 'must_select_questioncategory'] = 'Ви повинні вибрати категорію відповіді';
+$string[ 'must_select_quiz'] = 'Ви повинні вибрати вікторину';
+
+// File exporthtml_snakes.php.
+$string[ 'html_snakes_check'] = 'Перевірити';
+$string[ 'html_snakes_correct'] = 'Правильно!';
+$string[ 'html_snakes_no_selection'] = 'Доведеться вибрати що-небудь!';
+$string[ 'html_snakes_wrong'] = "Ваша відповідь неправильна. Знаходитесь на тому ж самому місці";
+$string[ 'score'] = 'Рахунок';
+
+// File index.php.
+$string[ 'helpbookquiz'] = 'Коли учень відповідає правильно може перейти до наступного розділу.';
+$string[ 'helpcross'] = 'Ця гра вимагає слова з будь-якого-глосарію або вікторини запитань, коротко відповідаєте на питання і система генерує випадковий кросворд. Вчитель може встановити максимальну кількість стовпців / рядків або слів, які містить кросворд. Учень може натиснути кнопку "Перевірити кросворд", для перевірки відповідей. Кожен кросворд генерується випадково, тобто відрізнятиметься для кожного учня..';
+$string[ 'helpcryptex'] = 'Ця гра, як кросворд але відповіді приховані всередині випадкового крипттексу винайденого Леонардо Да Вінчі.';
+$string[ 'helphangman'] = 'Ця гра вимагає слова з будь-якого глосарію або вікторини запитань. Вкажіть короткі відповіді на питання і система згенерує головоломку. Викладач може встановити кількість слів, які містяться в грі, з можливістю показати першу або останню букву, з можливістю показувати питання або відповідь в кінці.';
+$string[ 'helphiddenpicture'] = 'Частина прихованої картини відкривається при правильній відповіді студента. Кожна цифра містить питання, відповівіши правильно учень одержує частину прихованої картини.';
+$string[ 'helpmillionaire'] = 'Питання відображається студенту, по черзі, тобто, якщо правильно відповів отримуєш наступне запитання, до их пір поки не даси відповіді на всі запитання і не отримаєш головний приз. Якщо на питання відповів неправильно, гра закінчена.';
+$string[ 'helpsnakes'] = 'Відображається питання студенту, якщо правильно відповів, кидається кубик і гравець переміщається на число що випало на кубику.';
+$string[ 'helpsudoku'] = 'Ця гра показує судоку для студентів з недостатньою кількістю слів, щоб дозволити йому виграти. Для кожного питання студент правильно відповідає на додаткову кількість прорізів в головоломці, це зроблено для того що легше було виграти.';
+$string['modulename'] = 'Ігри';
+$string['modulename_help'] = 'Цей модуль містить 8 ігор: Анаграма, Кросворд, Криптекст, Мільйонер, Судлоку, Прихована картина, Догони лідера та Книга питань';
+$string[ 'modulenameplural'] = 'Ігри';
+$string[ 'pluginadministration'] = 'Управління грою';
+$string[ 'pluginname'] = 'Гра';
+
+// File lib.php.
+$string[ 'attempt'] = 'Спроби';
+$string[ 'bookquiz_questions'] = 'Асоціювати категорії питань до розділів книги';
+$string[ 'export_to_html'] = 'Експорт в HTML';
+$string[ 'export_to_javame'] = 'Експорт до Javame';
+$string[ 'game_bookquiz'] = 'Книга питань';
+$string[ 'game_cross'] = 'Кросворд';
+$string[ 'game_cryptex'] = 'Крипттекст';
+$string[ 'game_hangman'] = 'Анаграма';
+$string[ 'game_hiddenpicture'] = 'Прихована картина';
+$string[ 'game_millionaire'] = 'Мільйонер - своя гра';
+$string[ 'game_snakes'] = 'Догони лідера';
+$string[ 'game_sudoku'] = 'Судоку';
+$string[ 'info'] = 'Детально';
+$string[ 'noattempts'] = 'Ніяких спроб не було зроблено в цій грі';
+$string[ 'percent'] = 'Відсотків';
+$string[ 'reset_game_all'] = 'Видалення записів з усіх ігор';
+$string[ 'reset_game_deleted_course'] = 'Видалення записів з видалених курсів';
+$string[ 'results'] = 'Результати';
+$string[ 'showanswers'] = 'Показати відповіді';
+$string[ 'showattempts'] = 'Показати спроби';
+
+// File locallib.php.
+$string[ 'attemptfirst'] = 'Перша спроба';
+$string[ 'attemptlast'] = 'Остання спроба';
+$string[ 'convertfrom'] = '-';
+$string[ 'convertto'] = '-';
+$string[ 'gradeaverage'] = 'Середня оцінка';
+$string[ 'gradehighest'] = 'Найвища оцінка';
+
+// File mod_form.php.
+$string[ 'bookquiz_layout'] = 'Макет';
+$string[ 'bookquiz_layout0'] = 'Питання в верхній частині гри';
+$string[ 'bookquiz_layout1'] = 'Питання в нижній частині гри';
+$string[ 'bookquiz_options'] = 'Вибрати вікторину';
+$string[ 'bottomtext'] = 'Текст в нижній частині сторінки';
+$string[ 'cross_layout'] = 'Вигляд';
+$string[ 'cross_layout0'] = 'Поле для гри внизу кросворда';
+$string[ 'cross_layout1'] = 'Поле для гри внизу кросворда';
+$string[ 'cross_maxcols'] = 'Максимальна кількість стовпчиків/рядків';
+$string[ 'cross_max_attempts'] = 'Максимальна кількість спроб';
+$string[ 'cross_maxwords'] = 'Максимальна кількість слів';
+$string[ 'cross_minwords'] = 'Мінімальна кількість слів';
+$string[ 'cross_options'] = 'Налаштувати Кросворд';
+$string[ 'cryptex_maxtries'] = 'Кількість спроб';
+$string[ 'cryptex_options'] = 'Налаштувати Крипттекст';
+$string[ 'disablesummarize'] = 'Заборонити сумування';
+$string[ 'gameclose'] = 'Закрити гру';
+$string[ 'gameopen'] = 'Відкрити гру';
+$string[ 'gameopenclose'] = 'Дата відкриття і закриття';
+$string[ 'gameopenclose_help'] = 'Студенти можуть тільки почати  після вказаного часу, але вони повинні завершити всі свої спроби до вказаного часу.';
+$string[ 'grademethod'] = 'Спосіб оцінювання';
+$string[ 'hangman_allowspaces'] = 'Дозволити прогалини в словах';
+$string[ 'hangman_allowsub'] = 'Дозволити символ - в словах';
+$string[ 'hangman_imageset'] = 'Виберіть зображення для анаграми';
+$string[ 'hangman_language'] = 'Мова слів';
+$string[ 'hangman_maximum_number_of_errors'] = 'Перевищена максимальна кількість зображень (Зображення повинні названі так: hangman_0.jpg, hangman_1.jpg, ...)';
+$string[ 'hangman_maxtries'] = 'Кількість слів в грі';
+$string[ 'hangman_options'] = 'Налаштувати Анаграму';
+$string[ 'hangman_showcorrectanswer'] = 'Показати правильну відповідь після закінчення';
+$string[ 'hangman_showfirst'] = 'Показати першу букву анаграми';
+$string[ 'hangman_showlast'] = 'Показати останню букву анаграми';
+$string[ 'hangman_showquestion'] = 'Показати питання ?';
+$string[ 'language_user_defined'] = 'Користувач визначає мову';
+$string[ 'hiddenpicture_across'] = 'Клітинок по-горизонталі';
+$string[ 'hiddenpicture_down'] = 'Клітинок по-вертикалі';
+$string[ 'hiddenpicture_height'] = 'Встановіть висоту зображення (в пікселях)';
+$string[ 'hiddenpicture_options'] = 'Налаштувати Приховану картину';
+$string[ 'hiddenpicture_pictureglossary'] = 'Глосарій для питань картини';
+$string[ 'hiddenpicture_width'] = 'Встановити ширину зображення (в пікселях)';
+$string[ 'millionaire_background'] = 'Колір фону';
+$string[ 'millionaire_options'] = 'Налаштувати Мільйонер';
+$string[ 'millionaire_shuffle'] = 'Перемішати питання';
+$string[ 'snakes_background'] = 'Фон';
+$string[ 'snakes_cols'] = 'Стопвпчики';
+$string[ 'snakes_data'] = 'Позиція Змії та хід Лідера';
+$string[ 'snakes_file'] = 'Файл для фону';
+$string[ 'snakes_footerx'] = 'Відступ внизу зліва (в пікселях)';
+$string[ 'snakes_footery'] = 'Відступ внизу праворуч (iв пікселях)';
+$string[ 'snakes_headerx'] = 'Відступ вгору вліво (в пікселях)';
+$string[ 'snakes_headery'] = 'Відступ вгору вправо (в пікселях)';
+$string[ 'snakes_layout0'] = 'Питання в верхній частині зображення';
+$string[ 'snakes_layout1'] = 'Питання в нижній частині зображення';
+$string[ 'snakes_options'] = 'Налаштувати догони Лідера';
+$string[ 'snakes_rows'] = 'Рядки';
+$string[ 'sourcemodule'] = 'Джерело питань';
+$string[ 'sourcemodule_book'] = 'Виберіть книгу';
+$string[ 'sourcemodule_glossary'] = 'Виберіть глосарій';
+$string[ 'sourcemodule_glossarycategory'] = 'Виберіть категорію глосарію';
+$string[ 'sourcemodule_include_subcategories'] = 'Включити підкатегорії';
+$string[ 'sourcemodule_question'] = 'Питання';
+$string[ 'sourcemodule_questioncategory'] = 'Виберіть категорію запитань';
+$string[ 'sourcemodule_quiz'] = 'Виберіть вікторину';
+$string[ 'sudoku_maxquestions'] = 'Максимальна кількість питань';
+$string[ 'sudoku_options'] = 'Налаштувати Судоку';
+$string[ 'toptext'] = 'Текст у верхній частині сторінки';
+$string[ 'userdefined'] = 'Визначено користувачем';
+
+// File preview.php.
+$string[ 'only_teachers'] = 'Тільки вчителі можуть бачити цю сторінку';
+$string[ 'preview'] = 'Попередній перегляд';
+
+// File review.php.
+$string[ 'attempts'] = 'Спроби';
+$string[ 'completedon'] = 'Завершені';
+$string[ 'outof'] = '{$a->grade} із {$a->maxgrade} максимальних';
+$string[ 'review'] = 'Перегляд';
+$string[ 'reviewofattempt'] = 'Огляд спроб {$a}';
+$string[ 'showall'] = 'Показати всі';
+$string[ 'startagain'] = 'Почати заново';
+$string[ 'timetaken'] = 'Витрачений час';
+
+// File settings.php.
+$string[ 'hangmanimagesets'] = 'Кількість наборів зображень що використовується для анаграми';
+$string[ 'hidebookquiz'] = 'Приховати гру "Книга з питаннями"';
+$string[ 'hidecross'] = 'Приховати гру "Кросворд"';
+$string[ 'hidecryptex'] = 'Приховати гру "Крипттекст"';
+$string[ 'hidehangman'] = 'Приховати гру "Анаграму"';
+$string[ 'hidehiddenpicture'] = 'Приховати гру "Прихована картина"';
+$string[ 'hidemillionaire'] = 'Приховати гру "Мільйонер"';
+$string[ 'hidesnakes'] = 'Приховати гру "Гонка за лідером"';
+$string[ 'hidesudoku'] = 'Приховати гру "Судоку"';
+$string[ 'confighangmanimagesets'] = 'Налаштувати скількі зображень використовуватиметься для анаграми';
+$string[ 'confighidebookquiz'] = 'Показати "Книгу питань" вчителям';
+$string[ 'confighidecross'] = 'Показати "Кросворд" вчителям';
+$string[ 'confighidecryptex'] = 'Показати "Криптекст" вчителям';
+$string[ 'confighidehangman'] = 'Показати  "Анаграму" вчителям';
+$string[ 'confighidehiddenpicture'] = 'Показати "Прихована картина" вчителям';
+$string[ 'confighidemillionaire'] = 'Показати "Мільйонер" вчителям';
+$string[ 'confighidesnakes'] = 'Показати "Гонка за лідером" вчителям';
+$string[ 'confighidesudoku'] = 'Показати "Судоку" вчителям';
+
+// File showanswers.php.
+$string[ 'clearrepetitions'] = 'Скинути статистику';
+$string[ 'computerepetitions'] = 'Обчислити статистику зараз';
+$string[ 'feedbacks'] = 'Повідомлення про правильну відповідь';
+$string[ 'repetitions'] = 'Спроби';
+
+// File showattempts.php.
+$string[ 'lastip'] = 'IP гравця';
+$string[ 'showsolution'] = 'рішення';
+$string[ 'timefinish'] = 'Кінець гри';
+$string[ 'timelastattempt'] = 'Остання спроба';
+$string[ 'timestart'] = 'Старт';
+
+// File tabs.php.
+
+// File view.php.
+$string[ 'attemptgamenow'] = 'Розпочати гру зараз';
+$string[ 'comment'] = 'Коментар';
+$string[ 'continueattemptgame'] = 'Продовжити попередню спробу гри';
+$string[ 'gameclosed'] = 'Ця гра закрита з {$a}';
+$string[ 'gamecloseson'] = 'Ця гра закриється з {$a}';
+$string[ 'gamenotavailable'] = 'Гра буде недоступна до {$a}';
+$string[ 'gameopenedon'] = 'Ця гра відкрита з  {$a}';
+$string[ 'reattemptgame'] = 'Спробувати ще раз';
+$string[ 'yourfinalgradeis'] = 'Ваша фінальна оцінка в цій грі {$a}.';
